01ae0bacdd5ea335a42b9d3037a6ebc5bd5740f1
Ajay Mathew Joseph <ajay94.mec@gmail.com>
2024-11-19 23:50:02 +0100

    Changed from gmail to mailgun

--- main.py
+++ main.py
@@ -6,56 +6,27 @@ import base64
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
-from googleapiclient.http import MediaIoBaseDownload
-from google.oauth2.credentials import Credentials
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# Define the scope required by the Gmail API
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-
+# Your email credentials
 google_drive_file_id = os.environ.get("GDRIVE_FILE_ID")
-credentials = os.environ.get('GDRIVE_CREDENTIALS')
-token = os.environ.get('GDRIVE_TOKEN')
-token_json = json.loads(token)
-credentials_json = json.loads(credentials)
+MAIL_API = os.environ.get("MAIL_API")
+API_ENDPOINT = os.environ['API_ENDPOINT']
+SENDER = os.environ['SENDER']
+RECEIVER = os.environ['RECEIVER']
 
-def gmail_authenticate():
-    creds = None
-    # Load existing credentials if available
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_info(token_json)
-    # If there are no (valid) credentials, prompt the user to log in
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_config(
-            credentials_json, SCOPES
-        )
-            creds = flow.run_local_server(port=0)
-    return creds
+def send_email(sender_email, receiver_email, subject, body):
+    x = requests.post(
+  		API_ENDPOINT,
+  		auth=("api", MAIL_API),
+  		data={"from": f"GKCYL Koln <{sender_email}>",
+  			"to": [receiver_email],
+  			"subject": subject,
+  			"text": body})
+    print(x.text)
 
-def create_message(sender, to, subject, message_text):
-    # Compose the email as a MIMEText object
-    message = MIMEText(message_text)
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    return {'raw': raw}
-
-def send_email(service, sender, to, subject, message_text):
-    # Create and send the message
-    message = create_message(sender, to, subject, message_text)
-    try:
-        sent_message = service.users().messages().send(userId="me", body=message).execute()
-        print(f"Email sent successfully: {sent_message}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    
 def download_csv_from_drive(file_id):
     # Google Drive download URL with format
     download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
@@ -86,16 +57,13 @@ if __name__ == "__main__":
     # Check each entry in the CSV
     for _, row in birthdays.iterrows():
         if row['Birthday'] == tomorrow.strftime("%d-%m-%Y"):
-            creds = gmail_authenticate()  # Authenticate with Gmail API
-            service = build('gmail', 'v1', credentials=creds)  # Build Gmail API service
-
             # Set email details
-            sender_email = "gkcylberlin@gmail.com"
-            receiver_email = "gkcylkoln@gmail.com"
+            sender_email = SENDER
+            receiver_email = RECEIVER
             subject = "GKCYL Koln Unit Birthday Reminder"
             body = f"Tomorrow is the birthday of {row['Name']}"
 
             # Send the email
-            send_email(service, sender_email, receiver_email, subject, body)
+            send_email(sender_email, receiver_email, subject, body)
 
     shutil.rmtree('tmp')
